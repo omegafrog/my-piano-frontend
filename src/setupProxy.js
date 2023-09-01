8b137b6de4e81006e265782a8e70c2a18be282c7
omegafrog <jiwoo22110103@gmail.com>
2023-09-01 19:18:47 +0900

    logging request

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,6 +9,29 @@ module.exports = function (app) {
       pathRewrite: {
         "^/api": "/",
       },
+      onProxyReq: function onProxyReq(proxyReq, req, res) {
+        // Log outbound request to remote target
+        console.log(
+          "-->  ",
+          req.method,
+          req.path,
+          "->",
+          "headers:",
+          req.headers,
+          "proxyHeaders:",
+          proxyReq.getHeaders(),
+          "resHeaders:",
+          res.getHeaders(),
+          res.getHeaderNames()
+        );
+      },
+      onError: function onError(err, req, res) {
+        console.error(err);
+        res.status(500);
+        res.json({ error: "Error when connecting to remote server." });
+      },
+      logLevel: "debug",
+      secure: true,
     })
   );
 };

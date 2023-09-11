88bb3c59109df84084e580710206f9bfc7376eb1
omegafrog <jiwoo22110103@gmail.com>
2023-09-11 20:49:08 +0900

    use bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 

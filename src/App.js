f017c83bfbace9804a72c06a7e0f98e2dd31fb8c
omegafrog <jiwoo22110103@gmail.com>
2023-09-11 20:49:22 +0900

    add sheet route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Main from "./router/Main";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { UserProvider } from "./components/User-context";
+import Sheets from "./components/sheet/Sheets";
+import UploadSheet from "./components/sheet/UploadSheet";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/main" element={<Main />} />
         <Route path="/user/login" element={<Login />} />
         <Route path="/user/register" element={<Register />} />
+        <Route path="/sheet" element={<Sheets />} />
+        <Route path="/sheet/upload" element={<UploadSheet />} />
       </Routes>
     </UserProvider>
   );

64b92b671d18b55dc2ca02a5867d4b2906d245f4
omegafrog <jiwoo22110103@gmail.com>
2023-09-16 18:08:14 +0900

    upload file jsx component and renaming files

--- src/components/sheet/UploadFile.js
+++ src/components/sheet/UploadFile.js
@@ -1,4 +1,40 @@
-function UploadFile() {
+function UploadFile({ value }) {
+  const renameSheetImg = (event) => {
+    const files = event.target.files;
+    const form = new FormData();
+    for (const file of files) {
+      const nameList = file.name.split(".");
+      form.append(
+        "imgFiles",
+        new File(
+          [file],
+          "sheet-" +
+            nameList[0] +
+            "-" +
+            crypto.randomUUID() +
+            "." +
+            nameList[1],
+          { type: file.type }
+        )
+      );
+    }
+    const imgFiles = form.getAll("imgFiles");
+    let imageListItems = "";
+    imgFiles.forEach((sheet) => {
+      imageListItems += sheet.name;
+      imageListItems += ",";
+    });
+    console.log(imageListItems);
+    value.setSheetInfo((prev) => ({
+      ...prev,
+      sheetDto: {
+        ...prev.sheetDto,
+        filePath: imageListItems,
+        pageNum: files.length,
+      },
+    }));
+    return form;
+  };
   return (
     <form id="sheetForm" encType="multipart/form-data" method="POST">
       <div className="input-group mb-3">
@@ -7,6 +43,7 @@ function UploadFile() {
           className="form-control"
           id="inputGroupFile02"
           name="imgFiles"
+          onChange={renameSheetImg}
           multiple
         />
         <label className="input-group-text" htmlFor="inputGroupFile02">
@@ -16,4 +53,5 @@ function UploadFile() {
     </form>
   );
 }
+
 export default UploadFile;

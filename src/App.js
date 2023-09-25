3f086f3358d290d20722149fcbf5e5896bed8e1e
omegafrog <jiwoo22110103@gmail.com>
2023-09-25 18:26:45 +0900

    add google oauth context provider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Main from "./router/Main";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { UserProvider } from "./components/User-context";
 import Sheets from "./components/sheet/Sheets";
 import UploadSheet from "./components/sheet/UploadSheet";
+import SheetInfo from "./components/sheet/SheetInfo";
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <UserProvider>
-      <Routes>
-        <Route path="/main" element={<Main />} />
-        <Route path="/user/login" element={<Login />} />
-        <Route path="/user/register" element={<Register />} />
-        <Route path="/sheet" element={<Sheets />} />
-        <Route path="/sheet/upload" element={<UploadSheet />} />
-      </Routes>
-    </UserProvider>
+    <GoogleOAuthProvider clientId="25240194686-nd0b27v2dcvv9e458hhssip100630t74.apps.googleusercontent.com">
+      <Helmet></Helmet>
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to={"/main"} />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/user/login" element={<Login />} />
+            <Route path="/user/register" element={<Register />} />
+            <Route path="/sheet" element={<Sheets />} />
+            <Route path="/sheet/upload" element={<UploadSheet />} />
+            <Route path="/sheet/:id" element={<SheetInfo />} />
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
+    </GoogleOAuthProvider>
   );
 }
 

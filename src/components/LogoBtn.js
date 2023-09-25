6fc8bd3a8e2e25ecb51a5cbdbe58cea9d8761578
omegafrog <jiwoo22110103@gmail.com>
2023-09-25 18:25:39 +0900

    refactoring navbar logo img

--- src/components/LogoBtn.js
+++ src/components/LogoBtn.js
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom";
 function LogoBtn() {
   return (
     <div>
-      <Link to="/main">
-        <img src="img/logo-ko.png" alt="로고 이미지" />
-      </Link>
+      <img src="/img/logo-ko.png" alt="로고 이미지" />
     </div>
   );
 }

9fbd4c15fed4db7d14efcbd27112c9d032fe75f0
omegafrog <jiwoo22110103@gmail.com>
2023-09-25 18:20:54 +0900

    add sheet list display

--- src/components/sheet/Sheets.js
+++ src/components/sheet/Sheets.js
@@ -5,9 +5,27 @@ import axios from "axios";
 import { UserContext } from "../User-context";
 import { Link } from "react-router-dom";
 
+function SheetList({ sheetPosts }) {
+  if (sheetPosts.length > 0) {
+    return (
+      <div className="content m-4">
+        <ul>
+          {sheetPosts.map((item) => (
+            <li key={item.id}>
+              <a href={`/sheet/${item.id}`}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return <h1>아직 올린 악보가 없습니다.</h1>;
+  }
+}
+
 function Sheets() {
   const { accessToken } = useContext(UserContext);
-  const [sheetPosts, setSheetPosts] = useState([]);
+  const [sheetPosts, setSheetPosts] = useState();
 
   const getSheetPosts = async () => {
     const result = await axios.get("/api/sheet", { validateStatus: false });
@@ -21,20 +39,12 @@ function Sheets() {
   return (
     <div className="Sheets">
       <Navigator />
-      <div>
+      <div className="vh-100 d-flex">
         <LeftNavigator />
-        {sheetPosts.length > 0 ? (
-          <div className="content">
-            <ul>
-              {sheetPosts.map((item) => (
-                <li key={item.id}>
-                  <Link to={`${item.id}`}>{item.title}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : (
+        {sheetPosts === undefined ? (
           <h1>Loading...</h1>
+        ) : (
+          <SheetList sheetPosts={sheetPosts} />
         )}
       </div>
     </div>

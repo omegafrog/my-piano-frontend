f169264e7e9913628e491c4cd8fc0e38e6f19edb
omegafrog <jiwoo22110103@gmail.com>
2023-09-25 18:23:57 +0900

    set navigator user image size

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { UserContext } from "./User-context";
+import styles from "../css/UserInfo.module.css";
 
 function UserInfo() {
   const { loggedUser } = useContext(UserContext);
   // TODO : Iframe으로 유저 이름, 마이페이지,스크랩,로그아웃 버튼 띄우기
   return (
-    <div>
-      <img src={loggedUser.profileSrc} alt="user profile image" />;
-    </div>
+    <img
+      className={styles.Profile}
+      src={loggedUser.profileSrc}
+      alt="user profile"
+    />
   );
 }
 export default UserInfo;

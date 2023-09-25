04c4ab46867a7a98a3a71e0e04466e7d32ef822a
omegafrog <jiwoo22110103@gmail.com>
2023-09-25 18:25:22 +0900

    set navigator right img button size

--- src/components/NavigationBtn.js
+++ src/components/NavigationBtn.js
@@ -1,14 +1,12 @@
 import { Link } from "react-router-dom";
+import styles from "../css/NavigationBtn.module.css";
 
 function NavigationBtn({ text, img, href }) {
-  if (text === undefined) {
-    text = "";
+  if (text !== undefined) {
+    return { text };
   }
-  return (
-    <Link to={href}>
-      {text}
-      {img ? <img src={img} alt="button img"></img> : null}
-    </Link>
-  );
+  if (img !== undefined) {
+    return <img className={styles.NavBtn} src={img} alt="button img"></img>;
+  }
 }
 export default NavigationBtn;

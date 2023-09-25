9329adcadc45063cc308814a3bdd683959fce500
omegafrog <jiwoo22110103@gmail.com>
2023-09-25 18:26:22 +0900

    add google login button
    get user infomation token and send to backend.
    token is base64 encoded.

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,10 @@ import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "./User-context";
 import { useNavigate } from "react-router";
+import { Button, Form, Modal } from "react-bootstrap";
+import {  GoogleLogin } from "@react-oauth/google";
 
-function Login() {
+function Login({ show, handleClose }) {
   const [loginInfo, setLoginInfo] = useState({ username: "", password: "" });
   const url = "/api/user/login";
 
@@ -32,38 +34,73 @@ function Login() {
       });
       context.setAccessToken(accessToken);
       context.setLoggedIn(true);
+
       context.setLoggedUser(userInfoResponse.data.serializedData.user);
     }
   };
-  const navigate = useNavigate();
-  const submitLogin = (event) => {
-    event.preventDefault();
-    getLoginInfo();
-  };
 
+  const navigate = useNavigate();
   useEffect(() => {
     if (context.accessToken !== "") {
-      sessionStorage.setItem("userState", JSON.stringify(context));
+      localStorage.setItem("userState", JSON.stringify(context));
       navigate("/main");
     }
   }, [context.accessToken]);
 
   return (
-    <form onSubmit={submitLogin}>
-      <input
-        placeholder="Username"
-        id="username"
-        value={loginInfo.username}
-        onChange={getUsername}
-      />
-      <input
-        placeholder="Password"
-        id="password"
-        value={loginInfo.password}
-        onChange={getPassword}
-      />
-      <button>Login</button>
-    </form>
+    <Modal show={show} onHide={handleClose} animation={false}>
+      <Modal.Body className="d-grid gap-2">
+        <Form className="d-grid gap-2">
+          <Form.Control
+            placeholder="Username"
+            id="username"
+            value={loginInfo.username}
+            onChange={getUsername}
+          />
+          <Form.Control
+            placeholder="Password"
+            id="password"
+            value={loginInfo.password}
+            onChange={getPassword}
+          />
+          <Button
+            onClick={() => {
+              getLoginInfo();
+            }}
+          >
+            Login
+          </Button>
+        </Form>
+
+        <GoogleLogin
+          onSuccess={async (credentialResponse) => {
+            console.log(credentialResponse);
+            const response = await axios.post("/api/oauth2/google", {
+              code: credentialResponse.credential,
+            });
+            console.log("response:", response);
+            if (response.data.status === 302) {
+              const initialRegisterInfo = response.data.serializedData.userInfo;
+              navigate("/user/register", { state: initialRegisterInfo });
+            } else if (response.data.status === 200) {
+              console.log("로그인 성공");
+              const accessToken = response.data.serializedData["access token"];
+              const userInfoResponse = await axios.get("/api/user", {
+                headers: {
+                  Authorization: accessToken,
+                },
+              });
+              context.setAccessToken(accessToken);
+              context.setLoggedIn(true);
+              context.setLoggedUser(userInfoResponse.data.serializedData.user);
+            }
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </Modal.Body>
+    </Modal>
   );
 }
 

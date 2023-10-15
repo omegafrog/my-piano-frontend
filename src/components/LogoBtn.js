63b08e740040c35bd6cfbcab66b6c292af1045ba
omegafrog <jiwoo22110103@gmail.com>
2023-10-15 21:32:12 +0900

    logo size to static

--- src/components/LogoBtn.js
+++ src/components/LogoBtn.js
@@ -2,7 +2,7 @@ function LogoBtn() {
   return (
     <div>
       <img
-        style={{ height: "8vh", margin: "0px 2vh 0px 2vh" }}
+        style={{ height: "40px", margin: "0px 2vh 0px 2vh" }}
         src="/img/logo-ko.png"
         alt="로고 이미지"
       />

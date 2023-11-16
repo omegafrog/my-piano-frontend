2e14258a96102e7773fe421962c35571cca39a01
omegafrog <jiwoo22110103@gmail.com>
2023-11-16 16:24:42 +0900

    paymentModal창이 user상태에 따라 상태를 업데이트하도록 변경
    user가 Author 인 경우 -> 내 악보
    
    user가 이미 구매한 상품인 경우 -> 이미 구매하신 상품입니다.

---
+++ src/components/PaymentModal.js
@@ -0,0 +1,130 @@
+import { useContext, useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { UserContext } from "./User-context";
+import axios from "axios";
+
+export default function PaymentModal({ item }) {
+  const context = useContext(UserContext);
+  const [show, setShow] = useState(false);
+  const hideModal = () => setShow(false);
+  const showModal = () => {
+    if (context.loggedIn !== true) {
+      alert("로그인하고 이용해주세요.");
+    } else {
+      console.log("clicked");
+      console.log(context);
+      context.syncUserInfo();
+      setShow(true);
+    }
+  };
+
+  useEffect(() => {
+    const payBtn = document.querySelector("#payment-btn");
+    if (context.loggedUser.id === item.author.id) {
+      payBtn.innerHTML = "내 악보입니다.";
+      payBtn.disabled = true;
+      return;
+    } else if (context.loggedIn === false) {
+      payBtn.innerHTML = "구매하기";
+      payBtn.disabled = false;
+    }
+    axios
+      .get(`http://localhost:8080/order/sheet/${item.id}`, {
+        headers: {
+          Authorization: context.accessToken,
+        },
+        withCredentials: true,
+        validateStatus: false,
+      })
+      .then((response) => {
+        if (response.data.status === 200) {
+          if (response.data.serializedData.isOrdered === true) {
+            payBtn.innerHTML = "이미 구매하신 상품입니다.";
+            payBtn.disabled = true;
+          }
+        }
+      });
+  }, [context.loggedUser]);
+  const restCash = context.loggedUser.cash - item.price;
+  const purchase = () => {
+    axios
+      .post(
+        "http://localhost:8080/order/sheet",
+        {
+          itemId: item.id,
+          buyerId: context.loggedUser.id,
+        },
+        {
+          headers: {
+            Authorization: context.accessToken,
+          },
+          withCredentials: true,
+          validateStatus: false,
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        if (response.data.status === 200) {
+          hideModal();
+        }
+      })
+      .catch((error) => {
+        alert("error");
+        hideModal();
+      })
+      .finally(() => {
+        window.location.replace(`/sheet/${item.id}`);
+      });
+  };
+  const restCashColor = restCash < 0 ? "red" : "black";
+  return (
+    <>
+      <Button
+        variant="outline-danger"
+        className="w-100"
+        id="payment-btn"
+        size="lg"
+        style={{ margin: "5px" }}
+        onClick={showModal}
+      >
+        지금 구매
+      </Button>
+      <Modal show={show} onHide={hideModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <h3>{item.price === 0 ? "무료입니다." : `${item.price}원`}</h3>
+            <h5>{item.sheet.title}</h5>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="mx-4">
+          <div className="d-flex justify-content-between">
+            <span>보유 캐시</span>
+            <span>{context.loggedUser.cash}</span>
+          </div>
+          <div className="d-flex justify-content-between">
+            <span>차감될 캐시</span>
+            <span>{item.price}</span>
+          </div>
+          <hr></hr>
+          <div className="d-flex justify-content-between">
+            <span>남은 캐시</span>
+            <span style={{ color: restCashColor }}>
+              {context.loggedUser.cash - item.price}
+            </span>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          {restCash < 0 ? (
+            <Button variant="danger" disabled onClick={hideModal}>
+              잔액이 부족합니다
+            </Button>
+          ) : (
+            <Button variant="primary" onClick={purchase}>
+              구매 확정
+            </Button>
+          )}
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
--- src/components/User-context.js
+++ src/components/User-context.js
@@ -1,8 +1,28 @@
+import axios from "axios";
 import React, { useEffect, useMemo, useState } from "react";
+import { decodeToken } from "react-jwt";
 
 export const UserContext = React.createContext();
 
 export const UserProvider = ({ children }) => {
+  const syncUserInfo = (accessToken) => {
+    axios
+      .get("http://localhost:8080/user", {
+        headers: {
+          Authorization: accessToken,
+        },
+      })
+      .then((response) => {
+        if (response.data.status === 200) {
+          setState((prev) => ({
+            ...prev,
+            loggedUser: response.data.serializedData.user,
+            loggedIn: true,
+            accessToken: accessToken,
+          }));
+        }
+      });
+  };
   const setLoggedUser = (data) => {
     setState((prevState) => ({
       ...prevState,
@@ -23,9 +43,15 @@ export const UserProvider = ({ children }) => {
     }));
   };
   const initialize = () => {
-    setState(initialState);
+    setState((prev) => ({
+      ...prev,
+      loggedUser: {},
+      loggedIn: false,
+      accessToken: "",
+    }));
   };
-  let initialState = {
+
+  const initialState = {
     loggedUser: {},
     loggedIn: false,
     accessToken: "",
@@ -33,26 +59,31 @@ export const UserProvider = ({ children }) => {
     setLoggedIn,
     setAccessToken,
     initialize,
+    syncUserInfo,
   };
 
-  const [state, setState] = useState(initialState);
-
-  useEffect(() => {
-    if (localStorage.getItem("userState") !== null) {
-      setState({
-        ...JSON.parse(localStorage.getItem("userState")),
-        setLoggedIn,
-        setLoggedUser,
-        setAccessToken,
-        initialize,
-      });
-    }
-  }, []);
-
+  const [state, setState] = useState(
+    localStorage.getItem("userState") &&
+      JSON.parse(localStorage.getItem("userState")).loggedIn === true &&
+      Date.now() / 1000 <
+        decodeToken(JSON.parse(localStorage.getItem("userState")).accessToken)
+          .exp
+      ? {
+          ...JSON.parse(localStorage.getItem("userState")),
+          setLoggedIn,
+          setLoggedUser,
+          setAccessToken,
+          initialize,
+          syncUserInfo,
+        }
+      : initialState
+  );
   // state가 변경되면 localStorage에 자동으로 저장됨
   useEffect(() => {
     console.log("state change", state);
-    localStorage.setItem("userState", JSON.stringify(state));
+    if (state !== localStorage.getItem("userState")) {
+      localStorage.setItem("userState", JSON.stringify(state));
+    }
   }, [state]);
   const value = useMemo(() => state, [state]);
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;

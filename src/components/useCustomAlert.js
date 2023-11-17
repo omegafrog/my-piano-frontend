f19e2f55a27ffd0e4370b5b329732796d1021685
omegafrog <jiwoo22110103@gmail.com>
2023-11-17 17:28:53 +0900

    delete unused files

--- src/components/useCustomAlert.js
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-import CustomAlert from "./CustomAlert";
-
-export default function useCustomAlert() {
-  const [showAlert, setShowAlert] = useState({ state: false, text: "" });
-  
-  return <CustomAlert showAlert={showAlert} />;
-}

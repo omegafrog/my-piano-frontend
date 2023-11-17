a2a0c1a1c9ce667ca662b63309987718a3018bb8
omegafrog <jiwoo22110103@gmail.com>
2023-11-17 17:20:50 +0900

    add new route

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./css/App.css";
 import RegisterSuccessPage from "./components/user/RegisterSuccessPage";
-import NavSearchRecommend from "./components/NavSearchRecommend";
+import ScrappedSheetList from "./components/user/ScrappedSheetList";
+import SearchResult from "./components/SearchResult";
+import NotFound from "./components/NotFound";
+import Lessons from "./components/lesson/Lessons";
+import UploadLesson from "./components/lesson/UploadLesson";
+import LessonInfo from "./components/lesson/LessonInfo";
+import PurchasedSheets from "./components/user/PurchasedSheets";
 
 function App() {
   return (
@@ -25,11 +31,17 @@ function App() {
             <Route path="/sheet" element={<Sheets />} />
             <Route path="/sheet/upload" element={<UploadSheet />} />
             <Route path="/sheet/:id" element={<SheetInfo />} />
+            <Route path="/sheet/purchased" element={<PurchasedSheets />} />
+            <Route path="/lesson" element={<Lessons />} />
+            <Route path="/lesson/upload" element={<UploadLesson />} />
+            <Route path="/lesson/:id" element={<LessonInfo />} />
             <Route
               path="/user/register/success"
               element={<RegisterSuccessPage />}
             />
-            <Route path="/search/recommend" element={<NavSearchRecommend />} />
+            <Route path="/search" element={<SearchResult />} />
+            <Route path="/sheet/scrapped" element={<ScrappedSheetList />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </UserProvider>

e0482e8e36dd1a9c265723e35a8df22d70080dc0
omegafrog <jiwoo22110103@gmail.com>
2023-11-17 17:29:49 +0900

    user registration component

--- src/components/user/Register.js
+++ src/components/user/Register.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { Button, Form, Image, Row } from "react-bootstrap";
+import { Button, Container, Form, Image, Row } from "react-bootstrap";
 import CustomAlert from "../CustomAlert";
 
 function CheckPassword({ registerInfo, checkPassword }) {
@@ -30,16 +30,16 @@ function Register() {
   const [checkPassword, setCheckPassword] = useState("");
   const [showAlert, setShowAlert] = useState({ state: false, text: "" });
   const [profileFile, setProfileFile] = useState();
-
+  const googleSecret = process.env.REACT_APP_GOOGLE_API_SECRET;
   const [profileSrc, setProfileSrc] = useState("/img/defaultUserImg.png");
 
   useEffect(() => {
+    console.log(state);
     if (state !== null) {
-      fetch(state.profileSrc)
-        .then((response) => response.blob())
-        .then((blob) => {
-          setProfileSrc(URL.createObjectURL(blob));
-        });
+      if (state.loginMethod === "GOOGLE") {
+        console.log("his");
+        setProfileSrc(state.profileSrc + `?access_token=${googleSecret}`);
+      }
       state.phoneNum = "";
     }
   }, []);
@@ -68,7 +68,7 @@ function Register() {
     registerForm.append("profileImg", profileFile);
     const res = await axios({
       method: "post",
-      url: "/api/user/register",
+      url: "http://localhost:8080/user/register",
       withCredentials: true,
       data: registerForm,
     });
@@ -86,113 +86,112 @@ function Register() {
   };
 
   return (
-    <div
-      className="d-flex w-50 justify-content-center p-5 align-self-center"
-      style={{ backgroundColor: "#ecf0f1" }}
-    >
-      <CustomAlert value={{ showAlert, setShowAlert }} />
-      <Form className="w-100">
-        <Form.Group>
-          <Form.Label>아이디</Form.Label>
-          <Form.Control
-            type="text"
-            id="username"
-            value={registerInfo.username || ""}
-            onChange={(e) =>
-              setRegisterInfo({ ...registerInfo, username: e.target.value })
-            }
-            required
-          />
-          <Form.Text>로그인 시 사용되는 고유한 값입니다</Form.Text>
-        </Form.Group>
+    <Container fluid className="d-flex justify-content-center">
+      <div className="d-flex w-50  p-5 ">
+        <CustomAlert value={{ showAlert, setShowAlert }} />
+        <Form className="w-100">
+          <Form.Group>
+            <Form.Label>아이디</Form.Label>
+            <Form.Control
+              type="text"
+              id="username"
+              value={registerInfo.username || ""}
+              onChange={(e) =>
+                setRegisterInfo({ ...registerInfo, username: e.target.value })
+              }
+              required
+            />
+            <Form.Text>로그인 시 사용되는 고유한 값입니다</Form.Text>
+          </Form.Group>
 
-        <Form.Group>
-          <Form.Label>비밀번호</Form.Label>
-          <Form.Control
-            type="password"
-            id="password"
-            value={registerInfo.password || ""}
-            onChange={(e) =>
-              setRegisterInfo({ ...registerInfo, password: e.target.value })
-            }
-            required
-            disabled={registerInfo.loginMethod !== "EMAIL"}
-          />
-          <Form.Label>비밀번호 확인</Form.Label>
-          <Form.Control
-            type="password"
-            id="password-authetication"
-            value={checkPassword}
-            onChange={(e) => setCheckPassword(e.target.value)}
-            required
-            disabled={registerInfo.loginMethod !== "EMAIL"}
-          />
-          <CheckPassword
-            registerInfo={registerInfo}
-            checkPassword={checkPassword}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>이름</Form.Label>
-          <Form.Control
-            id="name"
-            value={registerInfo.name || ""}
-            onChange={(e) =>
-              setRegisterInfo({ ...registerInfo, name: e.target.value })
-            }
-            required
-          />
-        </Form.Group>
+          <Form.Group>
+            <Form.Label>비밀번호</Form.Label>
+            <Form.Control
+              type="password"
+              id="password"
+              value={registerInfo.password || ""}
+              onChange={(e) =>
+                setRegisterInfo({ ...registerInfo, password: e.target.value })
+              }
+              required
+              disabled={registerInfo.loginMethod !== "EMAIL"}
+            />
+            <Form.Label>비밀번호 확인</Form.Label>
+            <Form.Control
+              type="password"
+              id="password-authetication"
+              value={checkPassword}
+              onChange={(e) => setCheckPassword(e.target.value)}
+              required
+              disabled={registerInfo.loginMethod !== "EMAIL"}
+            />
+            <CheckPassword
+              registerInfo={registerInfo}
+              checkPassword={checkPassword}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>이름</Form.Label>
+            <Form.Control
+              id="name"
+              value={registerInfo.name || ""}
+              onChange={(e) =>
+                setRegisterInfo({ ...registerInfo, name: e.target.value })
+              }
+              required
+            />
+          </Form.Group>
 
-        <Form.Group>
-          <Form.Label>이메일</Form.Label>
-          <Form.Control
-            type="email"
-            id="email"
-            value={registerInfo.email || ""}
-            onChange={(e) =>
-              setRegisterInfo({ ...registerInfo, email: e.target.value })
-            }
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>전화번호</Form.Label>
-          <Form.Control
-            type="phone"
-            id="phoneNum"
-            value={registerInfo.phoneNum || ""}
-            onChange={(e) =>
-              setRegisterInfo({ ...registerInfo, phoneNum: e.target.value })
-            }
-          />
-        </Form.Group>
-        <Form.Group className="mb-3 d-flex flex-column ">
-          <Form.Label>프로필 사진</Form.Label>
-          <Image
-            src={profileSrc}
-            thumbnail
-            style={{ width: "200px", height: "200px", objectFit: "cover" }}
-            className="align-self-center"
-            id="profileImg"
-          />
-          <Form.Control type="file" onChange={changeProfile} />
-        </Form.Group>
-        <Row>
-          <Button className="m-2" onClick={submitRegister}>
-            회원 가입하기
-          </Button>
-          <Button
-            className="m-2"
-            variant="secondary"
-            onClick={() => {
-              navigate("/main");
-            }}
-          >
-            홈으로
-          </Button>
-        </Row>
-      </Form>
-    </div>
+          <Form.Group>
+            <Form.Label>이메일</Form.Label>
+            <Form.Control
+              type="email"
+              id="email"
+              value={registerInfo.email || ""}
+              onChange={(e) =>
+                setRegisterInfo({ ...registerInfo, email: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>전화번호</Form.Label>
+            <Form.Control
+              type="phone"
+              id="phoneNum"
+              value={registerInfo.phoneNum || ""}
+              onChange={(e) =>
+                setRegisterInfo({ ...registerInfo, phoneNum: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group className="mb-3 d-flex flex-column ">
+            <Form.Label>프로필 사진</Form.Label>
+            <Image
+              src={profileSrc}
+              thumbnail
+              style={{ width: "200px", height: "200px", objectFit: "cover" }}
+              className="align-self-center"
+              id="profileImg"
+            />
+            <Form.Control type="file" onChange={changeProfile} />
+          </Form.Group>
+          <Row>
+            <Button className="m-2" onClick={submitRegister}>
+              회원 가입하기
+            </Button>
+            <Button
+              className="m-2"
+              variant="secondary"
+              onClick={() => {
+                navigate("/main");
+              }}
+            >
+              홈으로
+            </Button>
+          </Row>
+        </Form>
+      </div>
+    </Container>
   );
 }
 

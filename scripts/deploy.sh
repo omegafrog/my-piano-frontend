43dfa95484f1da8609d76998bd8e6a403810a091
omegafrog <jiwoo22110103@gmail.com>
2024-03-30 15:18:10 +0900

    change web server to nginx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-pgrep -l http-server 2>/dev/null | awk '{print $1}' | sudo xargs kill
-sudo npx http-server ./ -S -C /etc/letsencrypt/live/mypiano.shop/cert.pem -K /etc/letsencrypt/live/mypiano.shop/privkey.pem
+sudo service nginx reload

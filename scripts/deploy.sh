0ffef1f4cf33e72de91adad1db4bf912f1de5926
omegafrog <jiwoo22110103@gmail.com>
2024-03-30 14:20:50 +0900

    add super user privilige npx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pgrep -l http-server 2>/dev/null | awk '{print $1}' | sudo xargs kill
-npx http-server ./ -S -C /etc/letsencrypt/live/mypiano.shop/cert.pem -K /etc/letsencrypt/live/mypiano.shop/privkey.pem
+sudo npx http-server ./ -S -C /etc/letsencrypt/live/mypiano.shop/cert.pem -K /etc/letsencrypt/live/mypiano.shop/privkey.pem

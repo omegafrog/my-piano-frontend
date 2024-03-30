4c3cabe4e848fc7291e9538bb6e6eddde5f95668
omegafrog <jiwoo22110103@gmail.com>
2024-03-30 13:43:42 +0900

    change deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-netstat -nap 2>/dev/null | grep :8080 | awk '{print $7}' | cut -d'/' -f1 | xargs kill
-
-npx serve -s ./ -p 8080
+pgrep -l http-server 2>/dev/null | awk '{print $1}' | sudo xargs kill
+npx http-server ./ -S -C /etc/letsencrypt/live/mypiano.shop/cert.pem -K /etc/letsencrypt/live/mypiano.shop/privkey.pem
